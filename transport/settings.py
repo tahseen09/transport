4289148ad6ce580f40fb102802b52bb3676052ea
Tahseen Rahman <tahseen.rahman@saarthi.ai>
2019-06-26 12:06:58 +0530

    Update trip func added

--- transport/settings.py
+++ transport/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'transport.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
--- transport/urls.py
+++ transport/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from truck import views
 
 urlpatterns = [
+    path('',views.show_trucks, name="show"),
     path('admin/', admin.site.urls),
 ]
