e6841d65e02e7d7d9179c4d7856a5bd54bbe4ee7
Tahseen Rahman <tahseen.rahman@saarthi.ai>
2019-07-01 10:58:23 +0530

    added Expenses table and view

--- truck/models.py
+++ truck/models.py
@@ -23,12 +23,25 @@ class Trip(models.Model):
     weight = models.FloatField(blank=True, null=True)
     cost_per_ton = models.FloatField(blank=True, null=True)
     total_cost = models.FloatField(blank=True, null=True)
-    comment = models.TextField(default="Trip Started:",blank=True, null=True)
-    expense = models.FloatField(default=0.00, blank=True, null=True)
+    #comment = models.TextField(default="Trip Started:",blank=True, null=True)
+    #expense = models.FloatField(default=0.00, blank=True, null=True)
     
     class Meta:
         unique_together = ["truck", "trip_start_date", "trip_start_time", "consignee", "driver"]
 
     def __str__(self):
         show = str(self.truck)+'|'+str(self.trip_start_date)+'|'+str(self.trip_start_time)
+        return show
+
+class Expenses(models.Model):
+    truck = models.CharField(blank=False, null=False, max_length = 40)
+    comment = models.TextField(blank=True, null=True)
+    expense = models.FloatField(default=0.00, blank=True, null=True)
+    expense_date = models.TimeField(blank=True, null=True)
+
+    class Meta:
+        unique_together = ["truck", "expense_date", "comment", "expense"]
+    
+    def __str__(self):
+        show = str(self.truck)+'|'+str(self.expense_date)
         return show
--- truck/views.py
+++ truck/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 import datetime
 from django.contrib.auth.decorators import login_required
-from .models import Trip
+from .models import Trip, Expenses
 
 
 def index(request):
@@ -70,17 +70,17 @@ def new_trip(request):
         consignee = request.POST.get("consignee")
         weight = request.POST.get("weight")
         cost = request.POST.get("cost")
-        comment = request.POST.get("comment")
-        expense = request.POST.get("expense")
+        #comment = request.POST.get("comment")
+        #expense = request.POST.get("expense")
         print(weight+' '+expense)
         if weight and ('.' not in weight):
             weight = int(weight)*1.0
         if cost and ('.' not in cost):
             cost = int(cost)*1.0
-        if expense and ('.' not in expense):
+        """if expense and ('.' not in expense):
             expense = int(expense)*1.0
         else:
-            expense = 0.0
+            expense = 0.0"""
         if weight and cost:
             weight = float(weight)
             cost = float(cost)
@@ -91,7 +91,7 @@ def new_trip(request):
             context = {"msg":msg}
             return render(request, "new.html", context)
         #save the data
-        t = Trip(truck=truck, trip_start_date=trip_start_date, trip_start_time=trip_start_time, source=source, destination=destination, driver=driver, item=item, consignee=consignee, weight=weight, cost_per_ton=cost, total_cost=total_cost, comment=comment, expense=expense)
+        t = Trip(truck=truck, trip_start_date=trip_start_date, trip_start_time=trip_start_time, source=source, destination=destination, driver=driver, item=item, consignee=consignee, weight=weight, cost_per_ton=cost, total_cost=total_cost)
         t.save()
         msg = "Your new trip has been created"
         context = {"msg":msg}
@@ -105,8 +105,8 @@ def update_trip(request):
     comm = ""
     if request.method == "POST":
         truck = request.POST.get("truck").upper()
-        expense = request.POST.get("expense")
-        comment = request.POST.get("comment")
+        #expense = request.POST.get("expense")
+        #comment = request.POST.get("comment")
         trip_end_date = request.POST.get("trip_end_date")
         trip_end_time = request.POST.get("trip_end_time")
         if not Trip.objects.filter(truck=truck, trip_complete=False).exists():
@@ -114,13 +114,13 @@ def update_trip(request):
             context = {"msg":msg}
             return render(request,"update.html",context)
         t = Trip.objects.filter(truck=truck, trip_complete=False)
-        if expense and ('.' not in expense):
+        """if expense and ('.' not in expense):
             expense = int(expense)*1.0
             print(expense)
             exp = t[0].expense+float(expense)
         if comment:
             comm = t[0].comment+'\n'+comment
-            Trip.objects.filter(truck=truck, trip_complete=False).update(expense=exp, comment=comm)
+            Trip.objects.filter(truck=truck, trip_complete=False).update(expense=exp, comment=comm)"""
         if trip_end_date:
             if trip_end_time:
                 Trip.objects.filter(truck=truck, trip_complete=False).update(trip_end_date=trip_end_date, trip_end_time=trip_end_time, trip_complete = True)
@@ -132,5 +132,22 @@ def update_trip(request):
     else:
         return render(request,"update.html")
 
+@login_required
 def expenses(request):
-    return render(request,"expenses.html")
+    expense = 0.0
+    if request.method == "POST":
+        truck = request.POST.get("truck").upper()
+        expense = request.POST.get("expense")
+        if expense and ('.' not in expense):
+            expense = int(expense)*1.0
+        else:
+            expense = float(expense)
+        comment = request.POST.get("comment")
+        expense_date = request.POST.get("expense_date")
+        t = Expenses(truck=truck, expense=expense, comment=comment, expense_date=expense_date)
+        t.save()
+        msg = "Expense saved successfully!"
+        context = {"msg":msg}
+        return render(request, "expenses.html", context)
+    else:
+        return render(request,"expenses.html")
